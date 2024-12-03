c4db0e8fd6e09243a2d42fd4df543edb4e639d2f
owner <ali-salehi@ali-Linux.digi.box>
2024-12-03 22:18:58 +0100

    010

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ resource "aws_instance" "Ali" {
     Created_By = "terraform"
   }
 }
+
+
+#terraform import aws_instance.Ali1 i-0bd09f036f02ad601
+resource "aws_instance" "Ali1" {
+  ami           = "ami-05edb7c94b324f73c"
+  instance_type = "t3.micro"
+  tags = {
+    Name       = "Ali1"
+    Created_By = "terraform"
+  }
+}
