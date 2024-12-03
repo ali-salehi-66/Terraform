0f791186382cc8ebf0cab8b575540c3c3cbb5f1f
owner <ali-salehi@ali-Linux.digi.box>
2024-12-03 11:21:55 +0100

    key,variable

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {}
+variable "instance_type" {
+
+}
+
+variable "key_name" {}
+variable "machine_name" {}
+variable "region" {}
+variable "backend_bucket" {}
+variable "backend_key" {}
